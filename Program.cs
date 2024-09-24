b73683c8366c0cebf718c8a6dbf039d108a3c1e5
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2024-09-24 13:26:16 +0200

    FIGHT

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿
 using System.Diagnostics.CodeAnalysis;
 
-int elisHP = 100;
-int teoHP = 100;
+int HeroHP = 100;
+int VillanHP = 100;
 int runda = 1;
 
          Random random = new Random();
@@ -17,25 +17,167 @@ int runda = 1;
     
     Console.WriteLine("Välkommen till spelet! Tryck på enter för att starta spelet!");
 
-    Console.ReadLine();
+    Console.WriteLine("Vill du spela på hard, easy eller normal svårihetsgrad?");
 
-    Console.WriteLine($"Elis har {elisHP} HP och Teo har {teoHP} HP");
+    
+    
+
+    while (HeroHP > 0 && VillanHP > 0) {
+
+    string mode = Console.ReadLine() ?? String.Empty;
 
 
+    if(mode.ToLower() == "normal") {
 
-    while (elisHP > 0 && teoHP > 0) {
+        while (HeroHP > 0 && VillanHP > 0) {
 
-        Console.WriteLine($"Runda {runda}!");
+
+       Console.WriteLine($"Runda {runda}!");
         elisDamage();
-         teoDamage();
+         NormalMode();
           runda++;
           Console.WriteLine("Tryck på enter för att börja nästa runda!");
           Console.ReadLine();
+         Console.Clear();
+        special();
 
-          Console.Clear();
+         
+        }
+
+     }
 
 
-          if(runda == 4) {
+     else if(mode.ToLower() == "hard") {
+
+        while (HeroHP > 0 && VillanHP > 0) {
+
+        
+        Console.WriteLine($"Runda {runda}!");
+        elisDamage();
+         HardMode();
+          runda++;
+          Console.WriteLine("Tryck på enter för att börja nästa runda!");
+          Console.ReadLine();
+         Console.Clear();
+         special();
+        
+        }
+
+    }
+
+
+    else if(mode.ToLower() == "easy") {
+
+        while (HeroHP > 0 && VillanHP > 0) {
+
+        
+        Console.WriteLine($"Runda {runda}!");
+        elisDamage();
+         EasyMode();
+          runda++;
+          Console.WriteLine("Tryck på enter för att börja nästa runda!");
+          Console.ReadLine();
+         Console.Clear();
+            special();
+        }
+
+    }
+
+
+    Console.WriteLine($"Hjälten har {HeroHP} HP och Skurken har {VillanHP} HP");
+
+
+
+   
+
+
+         
+
+    }
+    
+
+     if( HeroHP <= 0) {
+
+        Console.WriteLine("Skurken vann!");
+
+        slut();
+        
+
+    } else if(VillanHP <= 0) {
+
+        Console.WriteLine("Hjälten vann!");
+
+      slut();
+
+    }
+
+
+
+
+    
+
+    
+
+
+
+void elisDamage() {
+        
+        Random random = new Random();
+        int Edamage = Random.Shared.Next(1, 15);
+        VillanHP -= Edamage;
+        Console.WriteLine($" Skurken skadades {Edamage} och har nu {VillanHP} kvar!");
+
+    }
+
+    void NormalMode() {
+
+        Random random = new Random();
+        int Tdamage = Random.Shared.Next(1, 15);
+        HeroHP -= Tdamage;
+        Console.WriteLine($" Hjälten skadades {Tdamage} och har nu {HeroHP} kvar!");
+
+    }
+
+
+    void HardMode() {
+
+        Random random2 = new Random();
+        int Hskurk = Random.Shared.Next(10, 30);
+         HeroHP -= Hskurk;
+        Console.WriteLine($" Hjälten skadades {Hskurk} och har nu {HeroHP} kvar!");
+    }
+
+    
+    void EasyMode() {
+
+         Random random3 = new Random();
+        int Eskurk = Random.Shared.Next(1, 8);
+         HeroHP -= Eskurk;
+        Console.WriteLine($" Hjälten skadades {Eskurk} och har nu {HeroHP} kvar!");
+    }
+
+    void slut() {
+
+          Console.WriteLine("Vill du spela igen?");
+        string svar = Console.ReadLine() ?? string.Empty;
+        if(svar.ToLower() == "ja") {
+
+            HeroHP = 100;
+            VillanHP = 100;
+            runda = 1;
+
+
+        } else if(svar.ToLower() == "nej") {
+
+            Console.WriteLine("Synd! Du måste!");
+
+        }
+
+    }
+
+        void special() {
+
+             if(runda == 4) {
 
             Console.WriteLine("SPECIAL QUIZ ROUND!");
             Console.WriteLine($"Vad är {number} * {number2}?");
@@ -43,80 +185,19 @@ int runda = 1;
             string svar = Console.ReadLine() ?? string.Empty;
 
             if(svar == (number * number2).ToString()) {
-                teoHP -= 25;
-                Console.WriteLine("Rätt svar! Teo tog 25 CRITICAL HIT!");
+                VillanHP -= 25;
+                Console.WriteLine("Rätt svar! Skurken tog 25 CRITICAL HIT!");
             } else {
-                elisHP -= 25;
-                Console.WriteLine("Fel svar! DU TAR -25 CRITICAL HIT!");
+                HeroHP -= 25;
+                Console.WriteLine("Fel svar! Du tar -25 CRITICAL HIT!");
             } 
 
             }
 
-          }
-        
-
-     if( elisHP <= 0) {
-
-        Console.WriteLine("Teo vann!");
-
-        Console.WriteLine("Vill du spela igen?");
-        string svar = Console.ReadLine() ?? string.Empty;
-        if(svar.ToLower() == "ja") {
-
-            elisHP = 100;
-            teoHP = 100;
-            runda = 1;
-
-        } else if(svar.ToLower() == "nej") {
-
-            break;
-
-        }
-        
-
-    } else if(teoHP <= 0) {
-
-        Console.WriteLine("Elis vann!");
-
-        Console.WriteLine("Vill du spela igen?");
-        string svar = Console.ReadLine() ?? string.Empty;
-        if(svar.ToLower() == "ja") {
-
-            elisHP = 100;
-            teoHP = 100;
-            runda = 1;
-
-        } else if(svar.ToLower() == "nej") {
-
-            break;
 
         }
 
-    }
-
-
-
-
-    }
-
-void elisDamage() {
-
-        Random random = new Random();
-        int Edamage = Random.Shared.Next(1, 20);
-        teoHP -= Edamage;
-        Console.WriteLine($" teo tog {Edamage} och har nu {teoHP} kvar!");
-
-    }
-
-    void teoDamage() {
-
-        Random random = new Random();
-        int Tdamage = Random.Shared.Next(1, 20);
-        elisHP -= Tdamage;
-        Console.WriteLine($" elis tog {Tdamage} och har nu {elisHP} kvar!");
-
-    }
-
     
 
-    
+
+    }
